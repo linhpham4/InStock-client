03c1670d9d6a4251f1ebc6d6162db70410b71b7d
linhpham4 <linh.p44@gmail.com>
2024-09-04 14:25:57 -0500

    Update App.jsx to import header, footer, and all pages

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,37 @@ import Footer from './components/Footer/Footer'
 function App() {
   return (
     <>
-    <Header />
-      <Footer />
+       <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/warehouse" element={<WarehouseListPage />} />
+          <Route
+            path="/warehouse/:warehouseId/delete"
+            element={<WarehouseListPage />}
+          />
+          <Route
+            path="/warehouse/:warehouseId"
+            element={<WarehouseDetailsPage />}
+          />
+          <Route
+            path="/warehouse/:warehouseId/Edit"
+            element={<WarehouseEditPage />}
+          />
+          <Route path="/warehouse/add" element={<WarehouseAddPage />} />
+          <Route path="/inventory" element={<InventoryListPage />} />
+          <Route
+            path="/inventory/:itemId/delete"
+            element={<InventoryListPage />}
+          />
+          <Route path="/inventory/:itemId" element={<InventoryDetailsPage />} />
+          <Route
+            path="/inventory/:itemId/edit"
+            element={<InventoryEditPage />}
+          />
+          <Route path="/inventory/add" element={<InventoryAddPage />} />
+        </Routes>
+       <Footer />
+      </BrowserRouter>
     </>
   );
 }
