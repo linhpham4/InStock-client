c10f9bc1e9489c2f45b626db64258850beff1c12
ryanmody <rahimmody488@gmail.com>
2024-09-04 11:03:29 -0400

    Add array of object called warehouses to serve as dummy data until back-end is set up

---
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+const WarehouseList = () => {
+
+    const warehouses = [
+        {
+            warehouse: "Manhattan",
+            address: "503 Broadway, New York, USA",
+            contact: "Parmin Aujla",
+            number: "+1 (629) 555-0129",
+            email: "paujla@instock.com"
+        },
+        {
+            warehouse: "Washington",
+            address: "33 Pearl Street SW, Washington, USA",
+            contact: "Graeme Lyon",
+            number: "+1 (647) 504-0911",
+            email: "glyon@instock.com"
+        },
+        {
+            warehouse: "Jersey",
+            address: "300 Main Street, New Jersey, USA",
+            contact: "Brad MacDonald",
+            number: "+1 (401) 377-2337",
+            email: "bmcdonald@instock.com"
+        },
+        {
+            warehouse: "San Fran",
+            address: "890 Brannan Street, San Francisco, USA",
+            contact: "Gary Wong",
+            number: "+1 (239) 555-0108",
+            email: "gwong@instock.com"
+        },
+        {
+            warehouse: "Santa Monica",
+            address: "520 Broadway, Santa Monica, USA",
+            contact: "Sharon Ng",
+            number: "+1 (270) 555-0117",
+            email: "sng@instock.com"
+        },
+        {
+            warehouse: "Seattle",
+            address: "1201 Third Avenue, Seattle, USA",
+            contact: "Daniel Bachu",
+            number: "+1 (480) 555-0103",
+            email: "dbachu@instock.com"
+        },
+        {
+            warehouse: "Miami",
+            address: "2650 NW 5th Avenue, Miami, USA",
+            contact: "Alana Thomas",
+            number: "+1 (647) 832-2065",
+            email: "athomas@instock.com"
+        },
+    ]
+
+
+
+  return (
+    <>
+    
+    
+    
+    </>
+  )
+}
+
+export default WarehouseList
