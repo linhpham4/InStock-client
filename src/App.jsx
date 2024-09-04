51a49c250211f2d84711a9174cd228bd63486926
Studio117 <kwasiob@gmail.com>
2024-09-04 09:02:08 -0400

    added some placeholder css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.scss'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
   return (
     <>
-      
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.scss'
+
+// import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <p className='footer__copy'> <span className='footer__copyright-icon'>&copy; </span> InStock Inc. All Rights Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
