cbf1f03b16cdb681189203412a95887ca90064f0
ryanmody <rahimmody488@gmail.com>
2024-09-06 15:41:35 -0400

    Change scss styling names to match JSX file. Also moved the axios call to actual component, rather than the parent, since it's only needed in this particular component

--- src/components/WarehouseInventoryList/WarehouseInventoryList.jsx
+++ src/components/WarehouseInventoryList/WarehouseInventoryList.jsx
@@ -5,8 +5,26 @@ import editIcon from '../../assets/icons/edit-24px.svg'
 import sortIcon from '../../assets/icons/sort-24px.svg'
 import chevron from "../../assets/icons/chevron_right-24px.svg"
 import { Link } from 'react-router-dom'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom'
 
-const WarehouseInventoryList = ({ inventory }) => {
+const WarehouseInventoryList = () => {
+
+    const baseUrl = import.meta.env.VITE_API_URL
+
+    const [inventory, setInventory] = useState([])
+
+    const { warehouseId } = useParams();
+
+    async function getInventoryByWarehouse() {
+        const inventory = await axios.get(`${baseUrl}/stock//warehouses/${warehouseId}/inventories`)
+        setInventory(inventory.data)
+    }
+
+    useEffect(() => {
+        getInventoryByWarehouse();
+    }, [warehouseId]);
 
 if (!inventory) {
     return (
@@ -18,47 +36,47 @@ if (!inventory) {
     
         <main>
     
-        <div className='warehouse__headings'>
-            <div className='warehouse__title'>
+        <div className='inventory__headings'>
+            <div className='inventory__title'>
                 <p>INVENTORY ITEM</p>
-                <img className='warehouse__icon' src={sortIcon} alt="sort" />
+                <img className='inventory__icon' src={sortIcon} alt="sort" />
             </div>
-            <div className='warehouse__title'>
+            <div className='inventory__title'>
                 <p>CATEGORY</p>
-                <img className='warehouse__icon' src={sortIcon} alt="sort" />
+                <img className='inventory__icon' src={sortIcon} alt="sort" />
             </div>
-            <div className='warehouse__title'>
+            <div className='inventory__title'>
                 <p>STATUS</p>
-                <img className='warehouse__icon' src={sortIcon} alt="sort" />
+                <img className='inventory__icon' src={sortIcon} alt="sort" />
             </div>
-            <div className='warehouse__title'>
+            <div className='inventory__title'>
                 <p>QUANTITY</p>
-                <img className='warehouse__icon' src={sortIcon} alt="sort" />
+                <img className='inventory__icon' src={sortIcon} alt="sort" />
             </div>
-            <div className='warehouse__title'>
-                <p className='warehouse__title--padding'>ACTIONS</p>
+            <div className='inventory__title'>
+                <p className='inventory__title--padding'>ACTIONS</p>
             </div>
         </div>
     
     
         {inventory.map(item => (
     
-            <div key={item.id} className='warehouse'>
+            <div key={item.id} className='inventory'>
                     
-                <div className='warehouse__card'>
+                <div className='inventory__card'>
     
                     {/* This code will render at tablet/desktop breakpoints */}
-                    <Link className='warehouse__link toggle-tabletdesktop' to={`/warehouse/${item.id}`}>
-                        <p className='warehouse__name--blue '>{item.item_name}</p>
-                        <img className='warehouse__chevron' src={chevron} alt="chevron" />
+                    <Link className='inventory__link toggle-tabletdesktop' to={`/inventory/${item.id}`}>
+                        <p className='inventory__name--blue '>{item.item_name}</p>
+                        <img className='inventory__chevron' src={chevron} alt="chevron" />
                     </Link>
-                    <p className='warehouse__address toggle-tabletdesktop'>{item.category}</p>
-                    <p className='warehouse__name toggle-tabletdesktop'>{item.status}</p>
-                    <div className='warehouse__container toggle-tabletdesktop'>
-                        <p className='warehouse__address'>{item.quantity}</p>
+                    <p className='inventory__address toggle-tabletdesktop'>{item.category}</p>
+                    <p className='inventory__name toggle-tabletdesktop'>{item.status}</p>
+                    <div className='inventory__container toggle-tabletdesktop'>
+                        <p className='inventory__address'>{item.quantity}</p>
                     </div>
-                    <div className='warehouse__alticons toggle-tabletdesktop'>
-                        <img className='warehouse__altimages' src={deleteIcon} alt="delete" />
+                    <div className='inventory__alticons toggle-tabletdesktop'>
+                        <img className='inventory__altimages' src={deleteIcon} alt="delete" />
                         <img className='images' src={editIcon} alt="edit" />
                     </div>
                     {/* ---------------------------------------------------- */}
@@ -66,29 +84,29 @@ if (!inventory) {
     
     
                     {/* This code will render at mobile breakpoints */}
-                    <div className='warehouse__na toggle-mobile'>
-                        <div className='warehouse__container'>
-                            <p className='warehouse__label'>INVENTORY ITEM</p>
-                            <Link className='warehouse__link' to={`/warehouse/${item.id}`}> 
-                                <p className='warehouse__name--blue'>{item.item_name}</p>
-                                <img className='warehouse__chevron' src={chevron} alt="chevron" />
+                    <div className='inventory__na toggle-mobile'>
+                        <div className='inventory__container'>
+                            <p className='inventory__label'>INVENTORY ITEM</p>
+                            <Link className='inventory__link' to={`/inventory/${item.id}`}> 
+                                <p className='inventory__name--blue'>{item.item_name}</p>
+                                <img className='inventory__chevron' src={chevron} alt="chevron" />
                             </Link>
                         </div>
-                        <div className='warehouse__container'>
-                            <p className='warehouse__label'>CATEGORY</p>
-                            <p className='warehouse__address'>{item.category}</p>
+                        <div className='inventory__container'>
+                            <p className='inventory__label'>CATEGORY</p>
+                            <p className='inventory__address'>{item.category}</p>
                         </div>
                     </div>
     
-                    <div className='warehouse__contact toggle-mobile'>
+                    <div className='inventory__contact toggle-mobile'>
     
-                        <div className='warehouse__container'>
-                            <p className='warehouse__label'>STATUS</p>
-                            <p className='warehouse__name'>{item.status}</p>
+                        <div className='inventory__container'>
+                            <p className='inventory__label'>STATUS</p>
+                            <p className='inventory__name'>{item.status}</p>
                         </div>
-                        <div className='warehouse__container'>
-                            <p className='warehouse__label'>QTY</p>
-                            <p className='warehouse__info'>{item.quantity}</p>
+                        <div className='inventory__container'>
+                            <p className='inventory__label'>QTY</p>
+                            <p className='inventory__info'>{item.quantity}</p>
                         </div>
     
                     </div>
@@ -96,7 +114,7 @@ if (!inventory) {
     
                 </div>
     
-                <div className='warehouse__icons toggle-mobile'>
+                <div className='inventory__icons toggle-mobile'>
     
                     <img src={deleteIcon} alt="delete" />
                     <img src={editIcon} alt="edit" />
