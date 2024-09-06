2cd060e2ceee96121c864c7898713efc58e2b3da
ryanmody <rahimmody488@gmail.com>
2024-09-06 17:10:54 -0400

    Remove the extra slash

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -18,7 +18,7 @@ const InventoryList = () => {
     const { warehouseId } = useParams();
 
     async function getInventoryByWarehouse() {
-        const inventory = await axios.get(`${baseUrl}/stock//warehouses/${warehouseId}/inventories`)
+        const inventory = await axios.get(`${baseUrl}/stock/warehouses/${warehouseId}/inventories`)
         setInventory(inventory.data)
     }
 
@@ -26,16 +26,109 @@ const InventoryList = () => {
         getInventoryByWarehouse();
     }, [warehouseId]);
 
-
+    if (!inventory) {
+        return (
+            <div>Loading...</div>
+        )
+    } else {
 
   return (
 
+    <main>
+    
+    <div className='inventory__headings'>
+        <div className='inventory__title'>
+            <p>INVENTORY ITEM</p>
+            <img className='inventory__icon' src={sortIcon} alt="sort" />
+        </div>
+        <div className='inventory__title'>
+            <p>CATEGORY</p>
+            <img className='inventory__icon' src={sortIcon} alt="sort" />
+        </div>
+        <div className='inventory__title'>
+            <p>STATUS</p>
+            <img className='inventory__icon' src={sortIcon} alt="sort" />
+        </div>
+        <div className='inventory__title'>
+            <p>QUANTITY</p>
+            <img className='inventory__icon' src={sortIcon} alt="sort" />
+        </div>
+        <div className='inventory__title'>
+            <p className='inventory__title--padding'>ACTIONS</p>
+        </div>
+    </div>
 
 
-<div>placeholder</div>
+    {inventory.map(item => (
+
+        <div key={item.id} className='inventory'>
+                
+            <div className='inventory__card'>
+
+                {/* This code will render at tablet/desktop breakpoints */}
+                <Link className='inventory__link toggle-tabletdesktop' to={`/inventory/${item.id}`}>
+                    <p className='inventory__name--blue '>{item.item_name}</p>
+                    <img className='inventory__chevron' src={chevron} alt="chevron" />
+                </Link>
+                <p className='inventory__address toggle-tabletdesktop'>{item.category}</p>
+                <p className='inventory__name toggle-tabletdesktop'>{item.status}</p>
+                <div className='inventory__container toggle-tabletdesktop'>
+                    <p className='inventory__address'>{item.quantity}</p>
+                </div>
+                <div className='inventory__alticons toggle-tabletdesktop'>
+                    <img className='inventory__altimages' src={deleteIcon} alt="delete" />
+                    <img className='images' src={editIcon} alt="edit" />
+                </div>
+                {/* ---------------------------------------------------- */}
+
+
+
+                {/* This code will render at mobile breakpoints */}
+                <div className='inventory__na toggle-mobile'>
+                    <div className='inventory__container'>
+                        <p className='inventory__label'>INVENTORY ITEM</p>
+                        <Link className='inventory__link' to={`/inventory/${item.id}`}> 
+                            <p className='inventory__name--blue'>{item.item_name}</p>
+                            <img className='inventory__chevron' src={chevron} alt="chevron" />
+                        </Link>
+                    </div>
+                    <div className='inventory__container'>
+                        <p className='inventory__label'>CATEGORY</p>
+                        <p className='inventory__address'>{item.category}</p>
+                    </div>
+                </div>
+
+                <div className='inventory__contact toggle-mobile'>
+
+                    <div className='inventory__container'>
+                        <p className='inventory__label'>STATUS</p>
+                        <p className='inventory__name'>{item.status}</p>
+                    </div>
+                    <div className='inventory__container'>
+                        <p className='inventory__label'>QTY</p>
+                        <p className='inventory__info'>{item.quantity}</p>
+                    </div>
+
+                </div>
+                {/* ---------------------------------------------------- */}
+
+            </div>
+
+            <div className='inventory__icons toggle-mobile'>
+
+                <img src={deleteIcon} alt="delete" />
+                <img src={editIcon} alt="edit" />
+
+            </div>
+
+        </div>
+        ))}
 
     
-  )
+    </main>
+
+    
+  )}
 }
 
 export default InventoryList
