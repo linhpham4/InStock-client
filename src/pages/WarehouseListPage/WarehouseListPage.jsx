bab4c1094a3c5c23d0676317311cd3f82bdd9de7
Kyle Ingham <kyleingy@proton.me>
2024-09-07 16:24:11 -0600

    Made small adjustments to section component scss to fix error with component drifting outside of its container.

--- src/pages/WarehouseListPage/WarehouseListPage.jsx
+++ src/pages/WarehouseListPage/WarehouseListPage.jsx
@@ -3,10 +3,16 @@ import WarehouseList from "../../components/WarehouseList/WarehouseList";
 import SectionComponent from "../../components/SectionComponent/SectionComponent";
 
 function WarehouseListPage() {
+  const title = "Warehouses";
+  const buttonText = "+ Add New Warehouse";
   return (
     <>
-    {/* <SectionComponent /> */}
-      <WarehouseList />
+      <main className="section-header">
+        <SectionComponent title={title} buttonText={buttonText} />
+      </main>
+      <div className="warehouse-list__list-main">
+        <WarehouseList />
+      </div>
     </>
   );
 }
