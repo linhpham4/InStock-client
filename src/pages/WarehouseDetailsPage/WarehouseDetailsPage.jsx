ba43551fec369ea3ee0f77b73993e51342f136ff
Studio117 <kwasiob@gmail.com>
2024-09-06 20:24:33 -0400

    warehouses page done

--- src/pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx
+++ src/pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx
@@ -10,7 +10,7 @@ function WarehouseDetailsPage() {
 
       <SectionComponent2 />
       <WarehouseDetails />
-      <WarehouseInventoryList />
+      {/* <WarehouseInventoryList /> */}
     </div>
   );
 }
