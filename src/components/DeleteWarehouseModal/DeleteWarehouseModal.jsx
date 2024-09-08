d43f30199693201a75266330cf7bdc2dbb4a7daf
Kyle Ingham <kyleingy@proton.me>
2024-09-07 20:00:09 -0600

    Added delete modal to overlay page.

--- src/components/DeleteWarehouseModal/DeleteWarehouseModal.jsx
+++ src/components/DeleteWarehouseModal/DeleteWarehouseModal.jsx
@@ -6,23 +6,23 @@ function DeleteWarehouseModal({ Warehouse }) {
 
   return (
     <>
-      <section className="delete-wm">
-        <div className="delete-wm__container">
-          <div className="delete-wm__icon-container">
+      <section className="delete-wml">
+        <div className="delete-wml__container">
+          <div className="delete-wml__icon-container">
             <img
-              className="delete-wm__close-icon"
+              className="delete-wml__close-icon"
               src={close}
               alt="close tab"
             />
           </div>
-          <h1 className="delete-wm__title">Delete {Warehouse} Warehouse?</h1>
-          <p className="delete-wm__text">
+          <h1 className="delete-wml__title">Delete {Warehouse} Warehouse?</h1>
+          <p className="delete-wml__text">
             Please confirm that you'd like to delete {Warehouse} from the list
             of warehouses. You won't be able to undo this action.
           </p>
-          <div className="delete-wm__button-container">
-            <button className="delete-wm__button-1">Cancel</button>
-            <button className="delete-wm__button-2">Delete</button>
+          <div className="delete-wml__button-container">
+            <button className="delete-wml__button-1">Cancel</button>
+            <button className="delete-wml__button-2">Delete</button>
           </div>
         </div>
       </section>
