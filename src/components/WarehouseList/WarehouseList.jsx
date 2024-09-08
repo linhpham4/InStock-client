e6f029e62ce011d7707e0ccd8716e9dcb4b6e2db
Studio117 <kwasiob@gmail.com>
2024-09-08 13:04:07 -0400

    added warehouse deails to warehouse list page

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,71 +1,98 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import './WarehouseList.scss'
 import deleteIcon from '../../assets/icons/delete_outline-24px.svg'
 import editIcon from '../../assets/icons/edit-24px.svg'
 import sortIcon from '../../assets/icons/sort-24px.svg'
 import chevron from "../../assets/icons/chevron_right-24px.svg"
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+const URL = import.meta.env.VITE_APP_BASE_URL;
 
 const WarehouseList = () => {
 
-    const warehouses = [
-        {
-            warehouse: "Manhattan",
-            id: 1,
-            address: "503 Broadway, New York, USA",
-            contact: "Parmin Aujla",
-            number: "+1 (629) 555-0129",
-            email: "paujla@instock.com"
-        },
-        {
-            warehouse: "Washington",
-            id: 2,
-            address: "33 Pearl Street SW, Washington, USA",
-            contact: "Graeme Lyon",
-            number: "+1 (647) 504-0911",
-            email: "glyon@instock.com"
-        },
-        {
-            warehouse: "Jersey",
-            id: 3,
-            address: "300 Main Street, New Jersey, USA",
-            contact: "Brad MacDonald",
-            number: "+1 (401) 377-2337",
-            email: "bmcdonald@instock.com"
-        },
-        {
-            warehouse: "San Fran",
-            id: 4,
-            address: "890 Brannan Street, San Francisco, USA",
-            contact: "Gary Wong",
-            number: "+1 (239) 555-0108",
-            email: "gwong@instock.com"
-        },
-        {
-            warehouse: "Santa Monica",
-            id: 5,
-            address: "520 Broadway, Santa Monica, USA",
-            contact: "Sharon Ng",
-            number: "+1 (270) 555-0117",
-            email: "sng@instock.com"
-        },
-        {
-            warehouse: "Seattle",
-            id: 6,
-            address: "1201 Third Avenue, Seattle, USA",
-            contact: "Daniel Bachu",
-            number: "+1 (480) 555-0103",
-            email: "dbachu@instock.com"
-        },
-        {
-            warehouse: "Miami",
-            id: 7,
-            address: "2650 NW 5th Avenue, Miami, USA",
-            contact: "Alana Thomas",
-            number: "+1 (647) 832-2065",
-            email: "athomas@instock.com"
+    // const { id } = useParams();
+    // console.log(id)
+    // const [videos, setVideos] = useState([]);
+    const [warehouses, setWarehouses] = useState([]);
+
+    const getWarehouses = async () => {
+        try {
+            const results = await axios.get(`${URL}/stock/warehouses`)
+            const warehouses = results.data;
+            console.log(warehouses)
+            setWarehouses(warehouses);
+        } catch (error) {
+            console.log(error);
         }
-    ]
+    }
+
+    useEffect(() => {
+        getWarehouses();
+
+
+    }, []);
+
+
+
+    // const warehouses = [
+    //     {
+    //         warehouse: "Manhattan",
+    //         id: 1,
+    //         address: "503 Broadway, New York, USA",
+    //         contact: "Parmin Aujla",
+    //         number: "+1 (629) 555-0129",
+    //         email: "paujla@instock.com"
+    //     },
+    //     {
+    //         warehouse: "Washington",
+    //         id: 2,
+    //         address: "33 Pearl Street SW, Washington, USA",
+    //         contact: "Graeme Lyon",
+    //         number: "+1 (647) 504-0911",
+    //         email: "glyon@instock.com"
+    //     },
+    //     {
+    //         warehouse: "Jersey",
+    //         id: 3,
+    //         address: "300 Main Street, New Jersey, USA",
+    //         contact: "Brad MacDonald",
+    //         number: "+1 (401) 377-2337",
+    //         email: "bmcdonald@instock.com"
+    //     },
+    //     {
+    //         warehouse: "San Fran",
+    //         id: 4,
+    //         address: "890 Brannan Street, San Francisco, USA",
+    //         contact: "Gary Wong",
+    //         number: "+1 (239) 555-0108",
+    //         email: "gwong@instock.com"
+    //     },
+    //     {
+    //         warehouse: "Santa Monica",
+    //         id: 5,
+    //         address: "520 Broadway, Santa Monica, USA",
+    //         contact: "Sharon Ng",
+    //         number: "+1 (270) 555-0117",
+    //         email: "sng@instock.com"
+    //     },
+    //     {
+    //         warehouse: "Seattle",
+    //         id: 6,
+    //         address: "1201 Third Avenue, Seattle, USA",
+    //         contact: "Daniel Bachu",
+    //         number: "+1 (480) 555-0103",
+    //         email: "dbachu@instock.com"
+    //     },
+    //     {
+    //         warehouse: "Miami",
+    //         id: 7,
+    //         address: "2650 NW 5th Avenue, Miami, USA",
+    //         contact: "Alana Thomas",
+    //         number: "+1 (647) 832-2065",
+    //         email: "athomas@instock.com"
+    //     }
+    // ]
 
 
 
@@ -105,14 +132,14 @@ const WarehouseList = () => {
 
                         {/* This code will render at tablet/desktop breakpoints */}
                         <Link className='warehouse__link toggle-tabletdesktop' to={`/warehouse/${warehouse.id}`}>
-                            <p className='warehouse__name--blue '>{warehouse.warehouse}</p>
+                            <p className='warehouse__name--blue '>{warehouse.warehouse_name}</p>
                             <img className='warehouse__chevron' src={chevron} alt="chevron" />
                         </Link>
                         <p className='warehouse__address toggle-tabletdesktop'>{warehouse.address}</p>
-                        <p className='warehouse__name toggle-tabletdesktop'>{warehouse.contact}</p>
+                        <p className='warehouse__name toggle-tabletdesktop'>{warehouse.contact_name}</p>
                         <div className='warehouse__container toggle-tabletdesktop'>
-                            <p className='warehouse__address'>{warehouse.number}</p>
-                            <p className='warehouse__address'>{warehouse.email}</p>
+                            <p className='warehouse__address'>{warehouse.contact_phone}</p>
+                            <p className='warehouse__address'>{warehouse.contact_email}</p>
                         </div>
                         <div className='warehouse__alticons toggle-tabletdesktop'>
                             <img className='warehouse__altimages' src={deleteIcon} alt="delete" />
--- src/pages/WarehouseListPage/WarehouseListPage.jsx
+++ src/pages/WarehouseListPage/WarehouseListPage.jsx
@@ -3,11 +3,13 @@ import '../../App.scss'
 
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
 import SectionComponent from "../../components/SectionComponent/SectionComponent";
+import WarehouseDetails from "../../components/warehouseDetails/warehouseDetails";
 
 function WarehouseListPage() {
   return (
     <main>
     <SectionComponent title={"Warehouses"} buttonLink={"/warehouse/add"} buttonText={" Add New Warehouse"}/>
+    <WarehouseDetails />
       <WarehouseList />
     </main>
   );
